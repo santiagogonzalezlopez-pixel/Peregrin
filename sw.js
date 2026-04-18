65bd41ee05821ce2f78d7800722306983e992aac
santiagogonzalezlopez-pixel <santiago.gonzalez.lopez@gmail.com>
2026-04-18 20:45:34 +0200

    Improve auth flow and certificates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'peregrin-v2';
+const CACHE_NAME = 'peregrin-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/privacy.html', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
